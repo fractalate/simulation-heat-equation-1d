d7ff61b933dd194227a35118072431c5c5f085cd
Kate Novak <kje@cochleoid.com>
2024-05-15 15:48:42 -0400

    Correction on Method Used and Silver Example

--- README.md
+++ README.md
@@ -22,13 +22,16 @@ where $u(x, t)$ is a function describing the heat at position $x$ at time $t$ an
 
 * $u(x)$ is heat at position $x$ in the "rod".
 * $\alpha$ is the thermal diffusivity of the entire medium, which is constant.
-* The domain of $u$ is split into $N$ bins, each corresponding to a range of uniformly sized intervals around $x$. E.g. if there are $N$ bins and the total "rod" length is 1 cm, then the $k^{\text{th}}$ index satisfies
-  $\frac{1 \, \text{cm}}{N} \cdot k \le x_k < \frac{1 \, \text{cm}}{N} \cdot (k + 1)$.
-* The values for $u(x)$ are stored in an array indexed by $k$.
+* The domain of $u$ is split into $N$ bins, each corresponding to a range of uniformly sized intervals around $x$. E.g. if there are $N$ bins and the total "rod" length is 1 cm, then the $i^{\text{th}}$ index satisfies
+  $\frac{1 \, \text{cm}}{N} \cdot i \le x_i < \frac{1 \, \text{cm}}{N} \cdot (i + 1)$.
+* The values for $u(x)$ are stored in an array indexed by $i$.
+* The endpoints of the rod are assumed to be a fixed heat of $0$.
+* The small step in space is labeled $h$, but is named `dx` in the code.
+* The small step in time is labeled $k$, but is named `dt` in the code.
 
 ### Simulation
 
-The simulation implements the [Crank-Nicolson Method](https://en.wikipedia.org/wiki/Crank%E2%80%93Nicolson_method) to calculate the numeric approximations of the evolving heat dispersion over time. This method was chosen for its known stability and well-behaved error term, $O(dt^2 + dx^2)$ (TODO: make sure this is the case even though I deviated from the tridiagonal matrix setup and solving method presented in Burden and Faires Numerical Analysis, 9e).
+The simulation implements a [](https://en.wikipedia.org/wiki/Finite_difference_method) utilizing the backward difference to calculate the numeric approximations of the evolving heat dispersion over time. This method was chosen for its known stability and well-behaved error term, $O(k + h^2)$.
 
 ## Setup
 
@@ -38,13 +41,13 @@ The following Python libraries are required to run the simulation:
 pip install numpy scipy matplotlib
 ```
 
+See [requirements.txt](./requirements.txt) for full Python dependency details.
+
 Additionally, ffmpeg is required to render video files. You can get this on Debian with the following command:
 
 ```
 apt-get install ffmpeg
 ```
-
-See [requirements.txt](./requirements.txt) for full dependency details.
 
 ## Running
 
@@ -55,6 +58,12 @@ python3 simulate.py heat_diffusion
 ```
 
 This one will generate `heat_diffusion.mp4` which you can then watch.
+
+The following demos are available:
+
+* `basic`
+* `heat_diffusion`
+* `heat_diffusion_silver`
 
 ## Development
 
--- simulate.py
+++ simulate.py
@@ -4,6 +4,7 @@ import numpy as np
 from scipy import linalg
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
+from matplotlib.ticker import FuncFormatter
 
 # TODO: Rename this. This is confusable with sys.
 import system
@@ -25,7 +26,7 @@ class Simulation:
     def iterate(self):
         raise NotImplementedError()
 
-class SimulationCrankNicholson(Simulation):
+class SimulationBackwardDifference(Simulation):
     def __init__(self, samples: np.array, alpha: float, dx: float, dt: float):
         Simulation.__init__(self)
 
@@ -37,7 +38,7 @@ class SimulationCrankNicholson(Simulation):
         self.r: float = alpha * dt / dx**2
 
     def copy(self):
-        return SimulationCrankNicholson(self.samples.copy(), self.alpha, self.dx, self.dt)
+        return SimulationBackwardDifference(self.samples.copy(), self.alpha, self.dx, self.dt)
 
     def iterate(self):
         # Solve the tridiagonal matrix with linalg.solve_banded().
@@ -55,7 +56,7 @@ def setup_basic():
     LENGTH = 0.1
     SAMPLES = 10
     samples = system.example_two_separated_fifths(SAMPLES)
-    return SimulationCrankNicholson(
+    return SimulationBackwardDifference(
         samples,
         alpha = 1,
         dx = LENGTH / SAMPLES,
@@ -76,7 +77,7 @@ def setup_heat_diffusion():
     LENGTH = 0.1
     SAMPLES = 10000
     samples = system.example_two_separated_fifths(SAMPLES)
-    return SimulationCrankNicholson(
+    return SimulationBackwardDifference(
         samples,
         alpha = 1,
         dx = LENGTH / SAMPLES,
@@ -100,9 +101,50 @@ def demo_heat_diffusion():
     ani.save('heat_diffusion.mp4', writer='ffmpeg', fps=60)
     print('saved heat_diffusion.mp4')
 
+# TODO: Maybe make a class to track the samples and its parameters.
+#       There is a little conceptual dissonance since the simulation
+#       takes the alpha parameter, but it's really a property of the
+#       material too (and could vary, but the numerical methods also
+#       vary in that case too).
+SILVER_ROD_LENGTH = 0.10 # meters
+def setup_heat_diffusion_silver():
+    LENGTH = SILVER_ROD_LENGTH
+    SAMPLES = 10000
+    samples = system.example_two_separated_fifths(SAMPLES)
+    return SimulationBackwardDifference(
+        samples,
+        alpha = 1.6563e-4,
+        dx = LENGTH / SAMPLES,
+        dt = 1.0/60.0,
+    )
+
+def demo_heat_diffusion_silver():
+    sim = setup_heat_diffusion_silver()
+
+    fig, ax = plt.subplots()
+
+    # TODO: Port this kind of axis labeling to the other examples.
+    def scale_x(x, pos):
+        return '{:0.1f} cm'.format(x * sim.dx * SILVER_ROD_LENGTH * 100)
+
+    ax.xaxis.set_major_formatter(FuncFormatter(scale_x))
+
+    ax.set_title("Heat Diffusion (Silver)")
+    line, = ax.plot(sim.samples)
+
+    def update(frame_number):
+        sim.iterate()
+        line.set_ydata(sim.samples)
+        return line,
+
+    ani = FuncAnimation(fig, update, frames=600, blit=True)
+    ani.save('heat_diffusion_silver.mp4', writer='ffmpeg', fps=60)
+    print('saved heat_diffusion_silver.mp4')
+
 demos = {
     "basic": demo_basic,
-    "heat_diffusion": demo_heat_diffusion,  
+    "heat_diffusion": demo_heat_diffusion,
+    "heat_diffusion_silver": demo_heat_diffusion_silver,
 }
 
 def print_usage(file=None):
